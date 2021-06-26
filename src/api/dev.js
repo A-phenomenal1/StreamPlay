50e88847f3890466c1e9802cc844281d7f872e1e
A-phenomenal1 <niteshch200053@gmail.com>
2021-06-26 07:52:48 +0530

    ui-fixes

--- src/api/dev.js
+++ src/api/dev.js
@@ -1,7 +1,7 @@
 const dev = {
-  // BaseUrl: "https://streamplay-webapp.herokuapp.com/api",
-  BaseUrl: "http://localhost:3050/api",
-  port: 3050,
+  BaseUrl: "https://streamplay-webapp.herokuapp.com/api",
+  // BaseUrl: "http://localhost:3050/api",
+  // port: 3050,
 };
 
 export default dev;
